3b7f1a4098aa8a49d5dbd78ef0f12f36ce6d6309
Ashley Gudis <ashley.leick@gmail.com>
2023-03-09 19:59:31 -0600

    Adding Visualizations for Trip Analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,14 @@ reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearan
 
 # Determine p-value and r-squared value 
 summary(reg)
+
+# Read in CSV file 
+suspension <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+# Create a summary of the suspension coils PSI column 
+total_summary <- suspension %>% summarize(Mean = mean(PSI), Median = median(PSI),
+                                            Variance = var(PSI), SD = sd(PSI))
+
+# Create a summary with each manufacturing lot 
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), 
+                      Median = median(PSI),Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
